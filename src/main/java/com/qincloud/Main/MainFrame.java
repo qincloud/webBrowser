7244b2ab7b706d217c8c6147b002459bd30c017a
qincloud <2877408638@qq.com>
2017-04-18 21:18:05 +0800

    main代码修改

--- src/main/java/com/qincloud/Main/MainFrame.java
+++ src/main/java/com/qincloud/Main/MainFrame.java
@@ -1,13 +1,18 @@
 package com.qincloud.Main;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class MainFrame extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		
+		BorderPane root = new BorderPane();
+		Scene s = new Scene(root, 618, 382);
+		stage.setScene(s);
+		stage.show();
 	}
 
 	public static void main(String[] args) {
