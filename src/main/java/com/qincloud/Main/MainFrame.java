6c8358e95e8486390ab09490c1f5205d80956717
qincloud <2877408638@qq.com>
2017-04-18 21:14:33 +0800

    主文件代码提交

---
+++ src/main/java/com/qincloud/Main/MainFrame.java
@@ -0,0 +1,17 @@
+package com.qincloud.Main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainFrame extends Application {
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
